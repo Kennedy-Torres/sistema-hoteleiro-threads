760d14fc4d7e9d23bc8a6cae30345b24e4ee0bf0
Kennedy Rodrigo Torres Gonçalves <128331199+Kennedy-Torres@users.noreply.github.com>
2024-05-06 22:19:41 -0300

    Update Quarto.java
    inserção do syncronized no metodo de pegar e devolver chave p/ recepcao

--- sistema-hoteleiro/src/Quarto.java
+++ sistema-hoteleiro/src/Quarto.java
@@ -8,9 +8,9 @@ import java.util.concurrent.locks.ReentrantLock;
 // 1 quarto -> 1 chave
 // total de quartos = 10
 public class Quarto {
-    private int numeroQuarto; // mudar o nome para iDDoQuarto
+    private int numeroQuarto;
     private List<GrupoHospedes> grupos;
-    private boolean disponivel; // mudar o nome para quartoDisponivel ...em relação a reserva -> estar vago ou não
+    private boolean disponivel; //...em relação a reserva -> estar vago ou não
     private Lock lock;
     private boolean chaveNaRecepcao = true;
     private boolean TemGenteDentroDoQuarto = false;
@@ -34,7 +34,7 @@ public class Quarto {
             // Atualizamos o status de disponibilidade do quarto apenas se estava vazio antes de adicionar o grupo
             if (grupos.size() >= 1) {
                 disponivel = false; // O quarto deixa de estar disponível assim que um grupo é adicionado
-                chaveNaRecepcao = false; // o quarto é entrege ao grupo de hospedes logo a chave não está mais na recepção
+                chaveNaRecepcao = false; // o quarto é entregue ao grupo de hospedes logo a chave não está mais na recepção
                 TemGenteDentroDoQuarto = true;
             }
         }finally {
@@ -77,7 +77,7 @@ public class Quarto {
         return numeroQuarto;
     }
 
-    public synchronized boolean isChaveNaRecepcao() {
+    public synchronized boolean isChaveNaRecepcao() { 
         return chaveNaRecepcao;
     }
 
